4688b9dc3a28541778f4c56cca0fc8f23329a0b1
Emanuela <emanuela@github.com>
2018-03-24 20:04:39 +0100

    Class
    Class Econtact

---
+++ Econtact/Econtact/Econtact/contactClass/classContact.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Econtact.contactClass
+{
+    class classContact
+    {
+        //Getter setter proprertis
+        //Acts as Data Carrier in Our Application
+
+        public int ContactID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ContactNo { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+
+        static string myconnstring = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString;
+
+
+        //Selecting data from database
+        public DataTable Select()
+        {
+            //Step 1: database connection
+            SqlConnection conn = new SqlConnection(myconnstring);
+            DataTable dt = new DataTable();
+            try
+            {
+                //step 2: Writing SQL Query
+                string sql = "SELECT * FROM tblContact"; // tblContact from server explorer from tables
+
+                //creating cmd using sql and conn 
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //creating SQL adapter using cmd
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+
+
+        //Inserting data into Database
+        public bool Insert(classContact c)
+        {
+            //Creating a default return type and setting its value to false
+            bool isSuccess = false;
+
+            //Step 1: Connect database
+            SqlConnection conn = new SqlConnection(myconnstring);
+            try
+            {
+                //Step 2:Create a SQL Query to insert data
+                string sql = "INSERT INTO tblContact(FirstName, LastName, ContactNo, Adrress, Gender) VALUES(@FirstName, @LastName, @ContactNo, @Adrress, @Gender)";
+                //creating sql command using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Creatig parameters to add data
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+
+                //Connection open here
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //if the query runs successfully then the value of rows will be greater than zero its  value  will be 0
+                if(rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+
+
+        //Method to update data in database from our application 
+        public bool Update(classContact c)
+        {
+            //Create a default return type and set its default value to false
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstring);
+            try
+            {
+                //SQL to update data in our database
+                string sql = "UPDATE tblContact SET FirstName=@FirstName, LastName=@LastName, ContactNo=@ContactNo, Address=@Address, Gender=@Gender WHERE ContactID=@ContactID";
+
+                //Creating sql command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Create parameters to add value
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+                cmd.Parameters.AddWithValue("ContactID", c.ContactID);
+                //open database connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                //if the query runs sucessfully then the value of rows will be greater than zero else its value will be zero
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+
+
+        //Method to Delete Data from Database
+        public bool Delte(classContact c)
+        {
+            //Create a default return value and set its value to false
+            bool isSuccess = false;
+            //Create SQL connection 
+            SqlConnection conn = new SqlConnection(myconnstring);
+            try
+            {
+                //sql to delete data
+                string sql = "DELETE FROM tblContact WHERE ContactId=@ContactID";
+
+                //creating sql command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("ContactID", c.ContactID);
+                //open connection
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //If the query run sucessfully then the value od rows is the greather than zero else its value is 0
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                //Close connection
+                conn.Close();
+            }
+            return isSuccess;
+        }
+    }
+}
